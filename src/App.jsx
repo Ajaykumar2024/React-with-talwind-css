1a12de99f6a33884db7e87a8d2d88c21652ba5d0
Ajay Kumar <askajay025@gmail.com>
2026-04-09 22:21:01 +0530

    final build success

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,11 @@
     img: "https://plus.unsplash.com/premium_photo-1661630621969-6d9faac03f9f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D",
     introduction: "Neha is a startup founder working in fintech innovation.",
     tag1: "satisfied"
+  },
+  {
+    img: "https://images.unsplash.com/photo-1600275669283-4bf2bb8a990c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8d29ya2luZyUyMHByb2Zlc3Npb25hbHxlbnwwfHwwfHx8MA%3D%3D",
+    introduction: "Priya is a financial advisor helping small businesses manage their funds.",
+    tag1: "underbanked"
   }
 ];
    return (
